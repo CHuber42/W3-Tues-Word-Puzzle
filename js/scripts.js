d68cb4417b17a418ad522468e4a32428f316a97e
Christopher Huber <CHuber42.work@gmail.com>
2020-03-31 14:05:51 -0700

    Forgot to do commits while diagnosing; implemented for-looping

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,37 @@
 $(document).ready(function(){
+  $("form#inputForm").submit(function(event) {
+    event.preventDefault();
+    var inputSentence =$("input#inputSentence").val();
+    console.log(inputSentence);
 
-
-
-
-
-
-
-  
-
-  $("#output").textContent = ;
+    charactersToReplace = ["a", "e", "i", "o", "u"];
     
+    for (i=0; i <= inputSentence.length; i++) {
+ 
+      if (charactersToReplace.includes(inputSentence[i])){
+        $("#output").append("-"); 
+      }
+  
+      else {
+        $("#output").append(inputSentence[i]);
+      }
+
+    };
+
+    console.log(inputSentence);
+    
+  });
+
 });
+
+// $("#output").textContent = ;
+// for (i=0; i < charactersToReplace.length; i++) {
+   
+//   inputSentence.replace(charactersToReplace[i], "-");
+  
+//   }
+// 
+
+//if (inputSentence[i].includes(charactersToReplace)){}
+
+//if ("The Letter H" has somewhere in it all of the following: ("A, E, I, O, U")) Then:

11c9c00df7d5aec1aeb081474259fedf752a20fa
Christopher Huber <CHuber42.work@gmail.com>
2020-03-31 14:07:42 -0700

    remove some confusing comment text

--- js/scripts.js
+++ js/scripts.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
 
 });
 
-// $("#output").textContent = ;
+
 // for (i=0; i < charactersToReplace.length; i++) {
    
 //   inputSentence.replace(charactersToReplace[i], "-");
